f6e9186056ff1e7c4ef3199cb8e69bf0e21e48bb
HubertMelbruda <h.melbruda@gmail.com>
2022-05-05 16:48:19 +0200

    Add styles to search input and button.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,17 @@
   <div class="home">
     <div class="feature-card">
       <router-link to="/movie/tt0409591">
-        <img src="https://i.pinimg.com/564x/2a/2f/08/2a2f0865435e49294f00aeaae4ca9a2f.jpg" alt="Blade Runner" class="featured-img">
+        <img src="https://i.pinimg.com/564x/2a/2f/08/2a2f0865435e49294f00aeaae4ca9a2f.jpg" alt="Blade Runner" class="feature-card__img">
         <div class="detail">
-          <h3>Blade Runner</h3>
-          <p>Young Blade Runner K's discovery of a long-buried secret leads him to track down former Blade Runner Rick Deckard, who's been missing for thirty years.</p>
+          <h3 class="detail__header">Blade Runner</h3>
+          <p class="detail__info">Young Blade Runner K's discovery of a long-buried secret leads him to track down former Blade Runner Rick Deckard, who's been missing for thirty years.</p>
         </div>
       </router-link>
     </div>
     <form @submit.prevent class="search-box">
-      <input type="text" placeholder="What movie do you want to watch?">
-      <input type="submit" value="Search" class="search">
+      <input type="text" placeholder="What movie do you want to watch?" class="search-box__input search-box--reset">
+      <!-- <input type="submit" value="Search" class="search-button"> -->
+      <button type="submit" class="search-box__button search-box--reset">Search</button>
     </form>
 
     <div class="movies-list">MOVIES</div>
@@ -31,7 +32,7 @@ export default {
     .feature-card {
       position: relative;
 
-      .featured-img {
+      .feature-card__img {
         display: block;
         width: 100%;
         height: 300px;
@@ -39,6 +40,7 @@ export default {
         position: relative;
         z-index: 0;
       }
+
       .detail {
         position: absolute;
         left: 0;
@@ -48,13 +50,13 @@ export default {
         padding: 16px;
         z-index: 1;
 
-        h3 {
-          color:#FFF;
+        .detail__header {
+          color:#fff;
           margin-bottom: 16px;
         }
 
-        p {
-          color: #FFF;
+        .detail__info {
+          color: #fff;
         }
       }
     }
@@ -62,9 +64,52 @@ export default {
 
   .search-box {
     display: flex;
-    justify-content: center;
+    flex-direction: column;;
+
     align-content: center;
     align-items: center;
+    padding: 15px;
 
+    .search-box--reset {
+      display: block;
+      appearance: none;
+      border: none;
+      outline: none;
+      background: none; 
+    }
+
+    .search-box__input {
+      width: 100%;
+      color: #fff;
+      background-color: #496583;
+      text-align: center;
+      font-size: 18px;
+      padding: 10px 20px;
+      border-radius: 15px;
+
+      &::placeholder {
+        color: #FFF;
+      }
+
+      &:focus {
+        box-shadow: 0px 0px 10px #fff ;
+      }
+    }
+
+    .search-box__button {
+      width: 100%;
+      max-width: 200px;
+      color: #fff;
+      font-size: 20px;
+      background-color: #09a650;
+      padding: 10px 15px;
+      margin: 10px;
+      border-radius: 15px;
+
+      &:active {
+        background-color: #0bda68;
+        box-shadow: 0px 0px 10px #0bda68 ;
+      }
+    }
   }
 </style>
